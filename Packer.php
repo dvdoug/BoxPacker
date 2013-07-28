9c6f9fc083b5198eff6e904ec95cb8946e621ea8
Doug Wright <github@dougweb.org>
2013-07-28 18:37:39 +0100

    Add basic WIP packer
    Only works for determining a stack of items in a single box

---
+++ Packer.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Box packing (3D bin packing, knapsack problem)
+ * @package BoxPacker
+ * @author Doug Wright
+ */
+  namespace DVDoug\BoxPacker;
+
+  /**
+   * Actual packer
+   * @author Doug Wright
+   * @package BoxPacker
+   */
+  class Packer  {
+
+    /**
+     * List of items to be packed
+     * @var ItemList
+     */
+    protected $items;
+
+    /**
+     * List of box sizes available to pack items into
+     * @var Box[]
+     */
+    protected $boxes;
+
+    public function __construct() {
+      $this->items = new ItemList;
+      $this->boxes = new BoxList;
+    }
+
+    /**
+     * Add item to be packed
+     * @param Item $aItem
+     * @param int  $aQty
+     */
+    public function addItem(Item $aItem, $aQty = 1) {
+      for ($i = 0; $i < $aQty; $i++) {
+        $this->items->insert($aItem);
+      }
+    }
+
+    /**
+     * Add box size
+     * @param Box $aBox
+     */
+    public function addBox(Box $aBox) {
+      $this->boxes->insert($aBox);
+    }
+
+    /**
+     * Pack items into boxes
+     * @return PackedBox[]
+     */
+    public function pack() {
+      if (!sizeof($this->items)) {
+        throw new \RuntimeException('Please specify at least 1 item to be packed');
+      }
+
+      if (!sizeof($this->boxes)) {
+        throw new \RuntimeException('Please specify at least 1 size of box to pack items into');
+      }
+
+      $packedBoxes = [];
+      $unpackedItems = $this->items;
+      $boxesToEvaluate = clone $this->boxes;
+
+      while (!$boxesToEvaluate->isEmpty()) {
+        $packedItems = $this->packBox($boxesToEvaluate->top(), $unpackedItems);
+        $packedBoxes[] = new PackedBox($boxesToEvaluate->extract(), $packedItems);
+
+        /*
+         * Have we found a single box that contains everything?
+         */
+        if ($unpackedItems->count() == 0) {
+          return $packedBoxes;
+        }
+      }
+      throw new \RuntimeException('Could not find a single box large enough');
+    }
+
+
+    /**
+     * Pack as many items as possible into specific given box
+     * XXX for now, simply stacks items on top of each other, no side-by-side evaluation performed yet
+     * @param Box      $aBox
+     * @param ItemList $aItems
+     * @return ItemList items packed into box
+     */
+    public function packBox(Box $aBox, ItemList $aItems) {
+
+      $packedItems = new ItemList;
+      $remainingDepth = $aBox->getInnerDepth();
+      $remainingWeight = $aBox->getMaxWeight() - $aBox->getEmptyWeight();
+      while(!$aItems->isEmpty() && $aItems->top()->getDepth() <= $remainingDepth && $aItems->top()->getWeight() <= $remainingWeight) {
+        $itemD1 = $aItems->top()->getWidth();
+        $itemD2 = $aItems->top()->getLength();
+        $boxD1 = $aBox->getInnerWidth();
+        $boxD2 = $aBox->getInnerLength();
+
+        if (($itemD1 <= $boxD1 && $itemD2 <= $boxD2) || ($itemD2 <= $boxD1 && $itemD1 <= $boxD2)) { //check 2D rotation
+          $itemToPack = $aItems->extract();
+          $remainingDepth -= $itemToPack->getDepth();
+          $remainingWeight -= $itemToPack->getWeight();
+          $packedItems->insert($itemToPack);
+        }
+        else {
+          break;
+        }
+      }
+      return $packedItems;
+    }
+
+  }
---
+++ tests/PackerTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Doug
+ * Date: 28/07/13
+ * Time: 12:58
+ * To change this template use File | Settings | File Templates.
+ */
+
+  namespace DVDoug\BoxPacker;
+
+  class PackerTest extends \PHPUnit_Framework_TestCase {
+
+    public function testPackBoxThreeItemsFitEasily() {
+
+      $box = new TestBox('Le box', 300, 300, 10, 10, 296, 296, 8, 1000);
+
+      $items = new ItemList;
+      $items->insert(new TestItem('Item 1', 250, 250, 2, 200));
+      $items->insert(new TestItem('Item 2', 250, 250, 2, 200));
+      $items->insert(new TestItem('Item 3', 250, 250, 2, 200));
+
+      $packer = new Packer();
+      $packedItems = $packer->packBox($box, $items);
+
+      self::assertEquals(3, sizeof($packedItems));
+    }
+
+    public function testPackBoxThreeItemsFitExactly() {
+
+      $box = new TestBox('Le box', 300, 300, 10, 10, 296, 296, 8, 1000);
+
+      $items = new ItemList;
+      $items->insert(new TestItem('Item 1', 296, 296, 2, 200));
+      $items->insert(new TestItem('Item 2', 296, 296, 2, 500));
+      $items->insert(new TestItem('Item 3', 296, 296, 4, 290));
+
+      $packer = new Packer();
+      $packedItems = $packer->packBox($box, $items);
+
+      self::assertEquals(3, sizeof($packedItems));
+    }
+
+    public function testPackBoxThreeItemsFitSizeButOverweight() {
+
+      $box = new TestBox('Le box', 300, 300, 10, 10, 296, 296, 8, 1000);
+
+      $items = new ItemList;
+      $items->insert(new TestItem('Item 1', 250, 250, 2, 400));
+      $items->insert(new TestItem('Item 2', 250, 250, 2, 500));
+      $items->insert(new TestItem('Item 3', 250, 250, 2, 200));
+
+      $packer = new Packer();
+      $packedItems = $packer->packBox($box, $items);
+
+      self::assertEquals(2, sizeof($packedItems));
+    }
+
+    public function testPackBoxThreeItemsFitWeightBut2Oversize() {
+
+      $box = new TestBox('Le box', 300, 300, 10, 10, 296, 296, 8, 1000);
+
+      $items = new ItemList;
+      $items->insert(new TestItem('Item 1', 297, 296, 2, 200));
+      $items->insert(new TestItem('Item 2', 297, 296, 2, 500));
+      $items->insert(new TestItem('Item 3', 296, 296, 4, 290));
+
+      $packer = new Packer();
+      $packedItems = $packer->packBox($box, $items);
+
+      self::assertEquals(1, sizeof($packedItems));
+    }
+
+    public function testPackThreeItemsFitEasilyInSmallerOfTwoBoxes() {
+
+      $box1 = new TestBox('Le petite box', 300, 300, 10, 10, 296, 296, 8, 1000);
+      $box2 = new TestBox('Le grande box', 3000, 3000, 100, 100, 2960, 2960, 80, 10000);
+
+      $item1 = new TestItem('Item 1', 250, 250, 2, 200);
+      $item2 = new TestItem('Item 2', 250, 250, 2, 200);
+      $item3 = new TestItem('Item 3', 250, 250, 2, 200);
+
+      $packer = new Packer();
+      $packer->addBox($box1);
+      $packer->addBox($box2);
+      $packer->addItem($item1);
+      $packer->addItem($item2);
+      $packer->addItem($item3);
+      $packedBoxes = $packer->pack();
+
+      self::assertEquals(1, sizeof($packedBoxes));
+      self::assertEquals(3, $packedBoxes[0]->getItems()->count());
+      self::assertEquals($box1, $packedBoxes[0]->getBox());
+    }
+
+  }
