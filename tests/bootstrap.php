bfd1797e31825f277db383d61c627d26c858b630
Doug Wright <github@dougweb.org>
2013-11-27 16:29:49 +0000

    Remove old Travis workaround now fixed properly

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,15 +2,6 @@
 
   namespace DVDoug\BoxPacker;
 
-  require_once(__DIR__ . '/../Box.php');
-  require_once(__DIR__ . '/../BoxList.php');
-  require_once(__DIR__ . '/../Item.php');
-  require_once(__DIR__ . '/../ItemList.php');
-  require_once(__DIR__ . '/../PackedBox.php');
-  require_once(__DIR__ . '/../PackedBoxList.php');
-  require_once(__DIR__ . '/../Packer.php');
-  require_once(__DIR__ . '/../vendor/autoload.php');
-
  class TestBox implements Box {
 
    public function __construct($aReference, $aOuterWidth,$aOuterLength,$aOuterDepth,$aEmptyWeight,$aInnerWidth,$aInnerLength,$aInnerDepth,$aMaxWeight) {
