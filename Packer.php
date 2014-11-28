ae6466763b55170a50a2806a5355667ed7692080
Kevin Farley <kevin@ecommunities.ca>
2014-11-28 10:34:42 -0500

    - Added orientBox() and orientItem() public methods to Packer - Orient methods are backwards compatible and will test for setter support before proceeding, this was instead of modifying the interfaces of item and box to require them and thus breaking backward compatibility. - Orient methods will first test for optimal orientation (Depth is greatest, Length is smallest), and when not optimal will re-orient the box|item and reset it's applicable properties. - Currently only supported in addItem() and addBox()

--- Packer.php
+++ Packer.php
@@ -47,6 +47,7 @@
      * @param int  $aQty
      */
     public function addItem(Item $aItem, $aQty = 1) {
+      $aItem = $this->orientItem($aItem);
       for ($i = 0; $i < $aQty; $i++) {
         $this->items->insert($aItem);
       }
@@ -77,6 +78,7 @@
      * @param Box $aBox
      */
     public function addBox(Box $aBox) {
+      $abox = $this->orientBox($aBox);
       $this->boxes->insert($aBox);
       $this->logger->log(LogLevel::INFO, "added box {$aBox->getReference()}");
     }
@@ -89,6 +91,73 @@
       $this->boxes = clone $aBoxList;
     }
 
+    /**
+     * Optimize an box's orientation 
+     * @param box $box
+     * @return box
+     * @author Kevin Farley / eCommunties
+     */
+    public function orientBox(box $box) {
+    	// Ensure the proper methods exist for orientation reset, otherwise skip it.
+    	// FIXME: This can be eliminated in the future once the box interface is updated to include these methods (NB: which would break backward compatibility)
+    	if (!method_exists($box,'setInnerWidth') || !method_exists($box,'setInnerLength') || !method_exists($box,'setInnerDepth') || !method_exists($box,'setOuterWidth') || !method_exists($box,'setOuterLength') || !method_exists($box,'setOuterDepth')) {
+    		return $box;
+    	}
+    	
+    	// Extract the basic dimensions, break early if already oriented optimally
+    	if ($box->getOuterLength() <= $box->getOuterWidth() && $box->getOuterWidth() <= $box->getOuterDepth()) { 
+    		return $box;
+    	} else {
+    		$outer = array($box->getOuterLength(), $box->getOuterWidth(), $box->getOuterDepth());
+    		$inner = array($box->getInnerLength(), $box->getInnerWidth(), $box->getInnerDepth());
+    	}
+    		
+    	// Orient the box so that the longest edge is placed in depth, and then the smallest in length
+    	array_multisort($outer, $inner);
+    		
+    	// Reset the box dimensions
+    	$box->setOuterLength($outer[0]);
+    	$box->setOuterWidth($outer[1]);
+    	$box->setOuterDepth($outer[2]);
+    		
+    	$box->setInnerLength($inner[0]);
+    	$box->setInnerWidth($inner[1]);
+    	$box->setInnerDepth($inner[2]);
+
+    	return $box;
+    }
+    
+    /**
+     * Optimize an item's orientation 
+     * @param item $item
+     * @return item
+     * @author Kevin Farley / eCommunties
+     */
+    public function orientItem($item) {
+    	// Ensure the proper methods exist for orientation reset, otherwise skip it.
+    	// FIXME: This can be eliminated in the future once the item interface is updated to include these methods (NB: which would break backward compatibility)
+    	if (!method_exists($item,'setWidth') || !method_exists($item,'setLength') || !method_exists($item,'setDepth')) {
+    		return $item;
+    	}
+    	
+    	// Extract the basic dimensions, break early if already oriented optimally
+    	if ($item->getLength() <= $item->getWidth() && $item->getWidth() <= $item->getDepth()) { 
+    		return $item;
+    	} else {
+    		$measures = array($item->getLength(), $item->getWidth(), $item->getDepth());
+    	}
+    	
+    	// Orient the item so that the longest edge is placed in depth, and then the smallest in length
+    	sort($measures);
+    	
+    	// Reset the item dimensions
+    	$item->setLength($measures[0]);
+    	$item->setWidth($measures[1]);
+    	$item->setDepth($measures[2]);
+    	
+    	return $item;
+    }
+    
     /**
      * Pack items into boxes
      *
