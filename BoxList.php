89e9a7dd8d7b1ea4b7a073b15e1d0b93dea13cc7
IBBoard <dev@ibboard.co.uk>
2015-10-03 12:59:11 +0000

    Try to fix #30 by scaling our result by the min/max of our volumes

--- BoxList.php
+++ BoxList.php
@@ -6,19 +6,30 @@
  */
   namespace DVDoug\BoxPacker;
 
+  define('MAX_32BIT_INT', pow(2,31) - 1);
+
   /**
    * List of boxes available to put items into, ordered by volume
    * @author Doug Wright
    * @package BoxPacker
    */
   class BoxList extends \SplMinHeap {
+    private $min =  PHP_INT_MAX;
+    private $max =  0;
+
+    public function insert($value) {
+      $this->min = min($this->min, $value->getInnerVolume());
+      $this->max = max($this->max, $value->getInnerVolume());
+      parent::insert($value);
+    }
 
     /**
      * Compare elements in order to place them correctly in the heap while sifting up.
      * @see \SplMinHeap::compare()
      */
     public function compare($aBoxA, $aBoxB) {
-      return $aBoxB->getInnerVolume() - $aBoxA->getInnerVolume();
+      // Scale our result compared to our max and min volumes to never have integer overflow issues
+      return floor((($aBoxB->getInnerVolume() - $aBoxA->getInnerVolume()) / ($this->max - $this->min)) * MAX_32BIT_INT);
     }
 
   }
