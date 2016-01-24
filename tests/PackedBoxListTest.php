2b89467cbc498933021cdbe7be1a346b916a1d91
Doug Wright <github@dougweb.org>
2016-01-24 22:11:10 +0000

    Add unit tests for some getters

---
+++ tests/PackedBoxListTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Box packing (3D bin packing, knapsack problem)
+ * @package BoxPacker
+ * @author Doug Wright
+ */
+
+namespace DVDoug\BoxPacker;
+
+class PackedBoxListTest extends \PHPUnit_Framework_TestCase
+{
+    function testVolumeUtilisation()
+    {
+        $box = new TestBox('Box', 10, 10, 10, 10, 10, 10, 10, 10);
+        $item = new TestItem('Item', 5, 10, 10, 10);
+
+        $boxItems = new ItemList();
+        $boxItems->insert($item);
+
+        $packedBox = new PackedBox($box, $boxItems, 1, 2, 3, 4);
+
+        $packedBoxList = new PackedBoxList();
+        $packedBoxList->insert($packedBox);
+
+        self::assertEquals(50, $packedBoxList->getVolumeUtilisation());
+    }
+
+    function testWeightVariance()
+    {
+        $box = new TestBox('Box', 10, 10, 10, 10, 10, 10, 10, 10);
+        $item = new TestItem('Item', 5, 10, 10, 10);
+
+        $boxItems = new ItemList();
+        $boxItems->insert($item);
+
+        $packedBox = new PackedBox($box, $boxItems, 1, 2, 3, 4);
+
+        $packedBoxList = new PackedBoxList();
+        $packedBoxList->insert($packedBox);
+
+        self::assertEquals(0, $packedBoxList->getWeightVariance());
+    }
+}
---
+++ tests/PackedBoxTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Box packing (3D bin packing, knapsack problem)
+ * @package BoxPacker
+ * @author Doug Wright
+ */
+
+namespace DVDoug\BoxPacker;
+
+class PackedBoxTest extends \PHPUnit_Framework_TestCase
+{
+    function testGetters()
+    {
+        $box = new TestBox('Box', 370, 375, 60, 140, 364, 374, 40, 3000);
+        $item = new TestItem('Item', 230, 330, 6, 320);
+
+        $boxItems = new ItemList();
+        $boxItems->insert($item);
+
+        $packedBox = new PackedBox($box, $boxItems, 1, 2, 3, 4);
+
+        self::assertEquals(1, $packedBox->getRemainingWidth());
+        self::assertEquals(2, $packedBox->getRemainingLength());
+        self::assertEquals(3, $packedBox->getRemainingDepth());
+        self::assertEquals(4, $packedBox->getRemainingWeight());
+    }
+
+    function testVolumeUtilisation()
+    {
+        $box = new TestBox('Box', 10, 10, 10, 10, 10, 10, 10, 10);
+        $item = new TestItem('Item', 5, 10, 10, 10);
+
+        $boxItems = new ItemList();
+        $boxItems->insert($item);
+
+        $packedBox = new PackedBox($box, $boxItems, 1, 2, 3, 4);
+
+        self::assertEquals(50, $packedBox->getVolumeUtilisation());
+    }
+}
