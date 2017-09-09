6d78fd76964d44c8c4c6e2e59ce363e2143aa81c
Doug Wright <github@dougweb.org>
2017-09-09 14:34:59 +0100

    Call static methods statically

--- tests/PackerTest.php
+++ tests/PackerTest.php
@@ -382,7 +382,7 @@ class PackerTest extends TestCase
     {
         $packer = new Packer();
         $packer->setMaxBoxesToBalanceWeight(3);
-        $this->assertEquals(3, $packer->getMaxBoxesToBalanceWeight());
+        self::assertEquals(3, $packer->getMaxBoxesToBalanceWeight());
     }
 
     public function testWeightRedistributionActivatesUnderLimit()
@@ -395,8 +395,8 @@ class PackerTest extends TestCase
         $box1 = $packedBoxes->extract();
         $box2 = $packedBoxes->extract();
 
-        $this->assertEquals(2, $box1->getItems()->count());
-        $this->assertEquals(2, $box2->getItems()->count());
+        self::assertEquals(2, $box1->getItems()->count());
+        self::assertEquals(2, $box2->getItems()->count());
     }
 
     public function testWeightRedistributionDoesNotActivateOverLimit()
@@ -410,8 +410,8 @@ class PackerTest extends TestCase
         $box1 = $packedBoxes->extract();
         $box2 = $packedBoxes->extract();
 
-        $this->assertEquals(3, $box1->getItems()->count());
-        $this->assertEquals(1, $box2->getItems()->count());
+        self::assertEquals(3, $box1->getItems()->count());
+        self::assertEquals(1, $box2->getItems()->count());
     }
 
     /**
