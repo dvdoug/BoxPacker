7464a269ff619fb61a6f4d93d138b76cbe7824e8
Doug Wright <github@dougweb.org>
2017-09-09 23:26:51 +0100

    Round weight variance to 1dp

--- src/PackedBoxList.php
+++ src/PackedBoxList.php
@@ -94,7 +94,7 @@ class PackedBoxList extends \SplMinHeap
             $weightVariance += pow($box->getWeight() - $mean, 2);
         }
 
-        return $weightVariance / $this->count();
+        return round($weightVariance / $this->count(), 1);
 
     }
 
