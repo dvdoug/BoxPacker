d398fe535296abff54a6d75d379c251518a5a4b6
Doug Wright <github@dougweb.org>
2018-06-15 19:12:47 +0000

    Apply fixes from StyleCI

--- src/OrientatedItemFactory.php
+++ src/OrientatedItemFactory.php
@@ -233,9 +233,11 @@ class OrientatedItemFactory implements LoggerAwareInterface
     }
 
     /**
-     * Compare two items to see if they have same dimensions
+     * Compare two items to see if they have same dimensions.
+     *
      * @param Item $itemA
      * @param Item $itemB
+     *
      * @return bool
      */
     protected function isSameDimensions(Item $itemA, Item $itemB)
