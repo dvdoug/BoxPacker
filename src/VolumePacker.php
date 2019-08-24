1d342b782316513b10f80f2a8c9d746e522fa8aa
Doug Wright <github@dougweb.org>
2019-08-24 23:53:08 +0100

    CS fix

--- src/VolumePacker.php
+++ src/VolumePacker.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 namespace DVDoug\BoxPacker;
 
 use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
 use function count;
@@ -226,6 +225,7 @@ class VolumePacker implements LoggerAwareInterface
                 $this->logger->debug('no items fit, so starting next vertical layer');
                 $this->skippedItems[] = $itemToPack;
                 $this->rebuildItemList();
+
                 return;
             }
         }
