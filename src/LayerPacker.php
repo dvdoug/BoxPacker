0c8d35631b6ba4fd1bafe924baa9910861110287
Doug Wright <github@dougweb.org>
2020-04-12 00:44:48 +0100

    Split out layer packing code into it's own class

---
+++ src/LayerPacker.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * Box packing (3D bin packing, knapsack problem).
+ *
+ * @author Doug Wright
+ */
+declare(strict_types=1);
+
+namespace DVDoug\BoxPacker;
+
+use function count;
+use function max;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
+
+/**
+ * Layer packer.
+ *
+ * @internal
+ * @author Doug Wright
+ */
+class LayerPacker implements LoggerAwareInterface
+{
+    /**
+     * The logger instance.
+     *
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Box to pack items into.
+     *
+     * @var Box
+     */
+    private $box;
+
+    /**
+     * @var OrientatedItemFactory
+     */
+    private $orientatedItemFactory;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(Box $box)
+    {
+        $this->box = $box;
+
+        $this->logger = new NullLogger();
+
+        $this->orientatedItemFactory = new OrientatedItemFactory($this->box);
+        $this->orientatedItemFactory->setLogger($this->logger);
+    }
+
+    /**
+     * Sets a logger.
+     */
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
+        $this->orientatedItemFactory->setLogger($logger);
+    }
+
+    /**
+     * Pack items into an individual vertical layer.
+     */
+    public function packLayer(ItemList &$items, PackedItemList $packedItemList, array $layers, int $z, int $layerWidth, int $lengthLeft, int $depthLeft, int $guidelineLayerDepth, bool $singlePassMode): PackedLayer
+    {
+        $layers[] = $layer = new PackedLayer();
+        $prevItem = null;
+        $x = $y = $rowLength = 0;
+        $skippedItems = [];
+        $remainingWeightAllowed = $this->getRemainingWeightAllowed($layers);
+
+        while ($items->count() > 0) {
+            $itemToPack = $items->extract();
+
+            //skip items that are simply too heavy or too large
+            if (!$this->checkNonDimensionalConstraints($itemToPack, $layers, $remainingWeightAllowed, $packedItemList)) {
+                continue;
+            }
+
+            $orientatedItem = $this->orientatedItemFactory->getBestOrientation($itemToPack, $prevItem instanceof PackedItem ? $prevItem->toOrientatedItem() : null, $items, $layerWidth - $x, $lengthLeft, $depthLeft, $rowLength, $x, $y, $z, $packedItemList, $singlePassMode);
+
+            if ($orientatedItem instanceof OrientatedItem) {
+                $packedItem = PackedItem::fromOrientatedItem($orientatedItem, $x, $y, $z);
+                $layer->insert($packedItem);
+                $remainingWeightAllowed -= $itemToPack->getWeight();
+                $packedItemList->insert($packedItem);
+
+                $rowLength = max($rowLength, $orientatedItem->getLength());
+
+                //Figure out if we can stack the next item vertically on top of this rather than side by side
+                //e.g. when we've packed a tall item, and have just put a shorter one next to it.
+                $stackableDepth = ($guidelineLayerDepth ?: $layer->getDepth()) - $orientatedItem->getDepth();
+                $stackedZ = $z + $orientatedItem->getDepth();
+                $stackSkippedItems = [];
+                while ($stackableDepth > 0 && $items->count() > 0) {
+                    $itemToTryStacking = $items->extract();
+                    $stackedItem = $this->orientatedItemFactory->getBestOrientation($itemToTryStacking, $prevItem instanceof PackedItem ? $prevItem->toOrientatedItem() : null, $items, $orientatedItem->getWidth(), $orientatedItem->getLength(), $stackableDepth, $rowLength, $x, $y, $stackedZ, $packedItemList, $singlePassMode);
+                    if ($stackedItem && $this->checkNonDimensionalConstraints($itemToTryStacking, $layers, $remainingWeightAllowed, $packedItemList)) {
+                        $layer->insert(PackedItem::fromOrientatedItem($stackedItem, $x, $y, $stackedZ));
+                        $remainingWeightAllowed -= $itemToTryStacking->getWeight();
+                        $packedItemList->insert($packedItem);
+                        $stackableDepth -= $stackedItem->getDepth();
+                        $stackedZ += $stackedItem->getDepth();
+                    } else {
+                        $stackSkippedItems[] = $itemToTryStacking;
+                        // abandon here if next item is the same, no point trying to keep going. Last time is not skipped, need that to trigger appropriate reset logic
+                        while ($items->count() > 0 && static::isSameDimensions($itemToTryStacking, $items->top())) {
+                            $stackSkippedItems[] = $items->extract();
+                        }
+                    }
+                }
+                if ($stackSkippedItems) {
+                    $items = ItemList::fromArray(array_merge($stackSkippedItems, iterator_to_array($items)), true);
+                }
+                $x += $orientatedItem->getWidth();
+
+                $prevItem = $packedItem;
+                if ($items->count() === 0) {
+                    $items = ItemList::fromArray(array_merge($skippedItems, iterator_to_array($items)), true);
+                    $skippedItems = [];
+                }
+            } elseif (count($layer->getItems()) === 0) { // zero items on layer
+                $this->logger->debug("doesn't fit on layer even when empty, skipping for good");
+                continue;
+            } elseif ($items->count() > 0) { // skip for now, move on to the next item
+                $this->logger->debug("doesn't fit, skipping for now");
+                $skippedItems[] = $itemToPack;
+                // abandon here if next item is the same, no point trying to keep going. Last time is not skipped, need that to trigger appropriate reset logic
+                while ($items->count() > 2 && static::isSameDimensions($itemToPack, $items->top())) {
+                    $skippedItems[] = $items->extract();
+                }
+            } elseif ($x > 0) {
+                $this->logger->debug('No more fit in width wise, resetting for new row');
+                $lengthLeft -= $rowLength;
+                $y += $rowLength;
+                $x = $rowLength = 0;
+                $skippedItems[] = $itemToPack;
+                $items = ItemList::fromArray(array_merge($skippedItems, iterator_to_array($items)), true);
+                $skippedItems = [];
+                $prevItem = null;
+                continue;
+            } else {
+                $this->logger->debug('no items fit, so starting next vertical layer');
+                $skippedItems[] = $itemToPack;
+
+                $items = ItemList::fromArray(array_merge($skippedItems, iterator_to_array($items)), true);
+
+                return $layer;
+            }
+        }
+
+        return $layer;
+    }
+
+    /**
+     * As well as purely dimensional constraints, there are other constraints that need to be met
+     * e.g. weight limits or item-specific restrictions (e.g. max <x> batteries per box).
+     */
+    private function checkNonDimensionalConstraints(Item $itemToPack, array $layers, int $remainingWeightAllowed, PackedItemList $packedItemList): bool
+    {
+        $customConstraintsOK = true;
+        if ($itemToPack instanceof ConstrainedItem) {
+            $customConstraintsOK = $itemToPack->canBePackedInBox($packedItemList, $this->box);
+        }
+
+        return $customConstraintsOK && $itemToPack->getWeight() <= $remainingWeightAllowed;
+    }
+
+    private function getRemainingWeightAllowed(array $layers): int
+    {
+        $remainingWeightAllowed = $this->box->getMaxWeight() - $this->box->getEmptyWeight();
+        foreach ($layers as $layer) {
+            $remainingWeightAllowed -= $layer->getWeight();
+        }
+
+        return $remainingWeightAllowed;
+    }
+
+    /**
+     * Compare two items to see if they have same dimensions.
+     */
+    private static function isSameDimensions(Item $itemA, Item $itemB): bool
+    {
+        if ($itemA === $itemB) {
+            return true;
+        }
+        $itemADimensions = [$itemA->getWidth(), $itemA->getLength(), $itemA->getDepth()];
+        $itemBDimensions = [$itemB->getWidth(), $itemB->getLength(), $itemB->getDepth()];
+        sort($itemADimensions);
+        sort($itemBDimensions);
+
+        return $itemADimensions === $itemBDimensions;
+    }
+}
--- src/VolumePacker.php
+++ src/VolumePacker.php
@@ -9,7 +9,6 @@ declare(strict_types=1);
 namespace DVDoug\BoxPacker;
 
 use function count;
-use function max;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\NullLogger;
@@ -43,18 +42,20 @@ class VolumePacker implements LoggerAwareInterface
     protected $items;
 
     /**
-     * Whether the packer is in look-ahead mode (i.e. working ahead of the main packing).
+     * Whether the packer is in single-pass mode.
      *
      * @var bool
      */
     protected $singlePassMode = false;
 
     /**
-     * @var OrientatedItemFactory
+     * @var LayerPacker
      */
-    private $orientatedItemFactory;
+    private $layerPacker;
 
-    /** @var bool */
+    /**
+     * @var bool
+     */
     private $hasConstrainedItems;
 
     /**
@@ -69,8 +70,8 @@ class VolumePacker implements LoggerAwareInterface
 
         $this->hasConstrainedItems = $items->hasConstrainedItems();
 
-        $this->orientatedItemFactory = new OrientatedItemFactory($this->box);
-        $this->orientatedItemFactory->setLogger($this->logger);
+        $this->layerPacker = new LayerPacker($this->box);
+        $this->layerPacker->setLogger($this->logger);
     }
 
     /**
@@ -79,7 +80,7 @@ class VolumePacker implements LoggerAwareInterface
     public function setLogger(LoggerInterface $logger): void
     {
         $this->logger = $logger;
-        $this->orientatedItemFactory->setLogger($logger);
+        $this->layerPacker->setLogger($logger);
     }
 
     /**
@@ -134,7 +135,7 @@ class VolumePacker implements LoggerAwareInterface
      *
      * @return PackedBox packed box
      */
-    protected function packRotation(int $boxWidth, int $boxLength): PackedBox
+    private function packRotation(int $boxWidth, int $boxLength): PackedBox
     {
         $this->logger->debug("[EVALUATING ROTATION] {$this->box->getReference()}", ['width' => $boxWidth, 'length' => $boxLength]);
 
@@ -144,10 +145,11 @@ class VolumePacker implements LoggerAwareInterface
 
         while ($items->count() > 0) {
             $layerStartDepth = static::getCurrentPackedDepth($layers);
+            $packedItemList = $this->getPackedItemList($layers);
 
             //do a preliminary layer pack to get the depth used
             $preliminaryItems = clone $items;
-            $preliminaryLayer = $this->packLayer($preliminaryItems, $layers, $layerStartDepth, $boxWidth, $boxLength, $this->box->getInnerDepth() - $layerStartDepth, 0);
+            $preliminaryLayer = $this->layerPacker->packLayer($preliminaryItems, clone $packedItemList, $layers, $layerStartDepth, $boxWidth, $boxLength, $this->box->getInnerDepth() - $layerStartDepth, 0, $this->singlePassMode);
             if (count($preliminaryLayer->getItems()) === 0) {
                 break;
             }
@@ -156,7 +158,7 @@ class VolumePacker implements LoggerAwareInterface
                 $layers[] = $preliminaryLayer;
                 $items = $preliminaryItems;
             } else { //redo with now-known-depth so that we can stack to that height from the first item
-                $layers[] = $this->packLayer($items, $layers, $layerStartDepth, $boxWidth, $boxLength, $this->box->getInnerDepth() - $layerStartDepth, $preliminaryLayer->getDepth());
+                $layers[] = $this->layerPacker->packLayer($items, $packedItemList, $layers, $layerStartDepth, $boxWidth, $boxLength, $this->box->getInnerDepth() - $layerStartDepth, $preliminaryLayer->getDepth(), $this->singlePassMode);
             }
         }
 
@@ -172,109 +174,13 @@ class VolumePacker implements LoggerAwareInterface
     }
 
     /**
-     * Pack items into an individual vertical layer.
-     * @internal
-     */
-    protected function packLayer(ItemList &$items, array $layers, int $z, int $layerWidth, int $lengthLeft, int $depthLeft, int $guidelineLayerDepth): PackedLayer
-    {
-        $layers[] = $layer = new PackedLayer();
-        $prevItem = null;
-        $x = $y = $rowLength = 0;
-        $skippedItems = [];
-        $remainingWeightAllowed = $this->getRemainingWeightAllowed($layers);
-        $packedItemList = $this->getPackedItemList($layers);
-
-        while ($items->count() > 0) {
-            $itemToPack = $items->extract();
-
-            //skip items that are simply too heavy or too large
-            if (!$this->checkNonDimensionalConstraints($itemToPack, $layers, $remainingWeightAllowed)) {
-                continue;
-            }
-
-            $orientatedItem = $this->orientatedItemFactory->getBestOrientation($itemToPack, $prevItem instanceof PackedItem ? $prevItem->toOrientatedItem() : null, $items, $layerWidth - $x, $lengthLeft, $depthLeft, $rowLength, $x, $y, $z, $packedItemList, $this->singlePassMode);
-
-            if ($orientatedItem instanceof OrientatedItem) {
-                $packedItem = PackedItem::fromOrientatedItem($orientatedItem, $x, $y, $z);
-                $layer->insert($packedItem);
-                $remainingWeightAllowed -= $itemToPack->getWeight();
-                $packedItemList->insert($packedItem);
-
-                $rowLength = max($rowLength, $orientatedItem->getLength());
-
-                //Figure out if we can stack the next item vertically on top of this rather than side by side
-                //e.g. when we've packed a tall item, and have just put a shorter one next to it.
-                $stackableDepth = ($guidelineLayerDepth ?: $layer->getDepth()) - $orientatedItem->getDepth();
-                $stackedZ = $z + $orientatedItem->getDepth();
-                $stackSkippedItems = [];
-                while ($stackableDepth > 0 && $items->count() > 0) {
-                    $itemToTryStacking = $items->extract();
-                    $stackedItem = $this->orientatedItemFactory->getBestOrientation($itemToTryStacking, $prevItem instanceof PackedItem ? $prevItem->toOrientatedItem() : null, $items, $orientatedItem->getWidth(), $orientatedItem->getLength(), $stackableDepth, $rowLength, $x, $y, $stackedZ, $packedItemList, $this->singlePassMode);
-                    if ($stackedItem && $this->checkNonDimensionalConstraints($itemToTryStacking, $layers, $remainingWeightAllowed)) {
-                        $layer->insert(PackedItem::fromOrientatedItem($stackedItem, $x, $y, $stackedZ));
-                        $remainingWeightAllowed -= $itemToTryStacking->getWeight();
-                        $packedItemList->insert($packedItem);
-                        $stackableDepth -= $stackedItem->getDepth();
-                        $stackedZ += $stackedItem->getDepth();
-                    } else {
-                        $stackSkippedItems[] = $itemToTryStacking;
-                        // abandon here if next item is the same, no point trying to keep going. Last time is not skipped, need that to trigger appropriate reset logic
-                        while ($items->count() > 0 && static::isSameDimensions($itemToTryStacking, $items->top())) {
-                            $stackSkippedItems[] = $items->extract();
-                        }
-                    }
-                }
-                if ($stackSkippedItems) {
-                    $items = ItemList::fromArray(array_merge($stackSkippedItems, iterator_to_array($items)), true);
-                }
-                $x += $orientatedItem->getWidth();
-
-                $prevItem = $packedItem;
-                if ($items->count() === 0) {
-                    $items = ItemList::fromArray(array_merge($skippedItems, iterator_to_array($items)), true);
-                    $skippedItems = [];
-                }
-            } elseif (count($layer->getItems()) === 0) { // zero items on layer
-                $this->logger->debug("doesn't fit on layer even when empty, skipping for good");
-                continue;
-            } elseif ($items->count() > 0) { // skip for now, move on to the next item
-                $this->logger->debug("doesn't fit, skipping for now");
-                $skippedItems[] = $itemToPack;
-                // abandon here if next item is the same, no point trying to keep going. Last time is not skipped, need that to trigger appropriate reset logic
-                while ($items->count() > 2 && static::isSameDimensions($itemToPack, $items->top())) {
-                    $skippedItems[] = $items->extract();
-                }
-            } elseif ($x > 0) {
-                $this->logger->debug('No more fit in width wise, resetting for new row');
-                $lengthLeft -= $rowLength;
-                $y += $rowLength;
-                $x = $rowLength = 0;
-                $skippedItems[] = $itemToPack;
-                $items = ItemList::fromArray(array_merge($skippedItems, iterator_to_array($items)), true);
-                $skippedItems = [];
-                $prevItem = null;
-                continue;
-            } else {
-                $this->logger->debug('no items fit, so starting next vertical layer');
-                $skippedItems[] = $itemToPack;
-
-                $items = ItemList::fromArray(array_merge($skippedItems, iterator_to_array($items)), true);
-
-                return $layer;
-            }
-        }
-
-        return $layer;
-    }
-
-    /**
      * During packing, it is quite possible that layers have been created that aren't physically stable
      * i.e. they overhang the ones below.
      *
      * This function reorders them so that the ones with the greatest surface area are placed at the bottom
      * @param PackedLayer[] $layers
      */
-    protected static function stabiliseLayers(array $layers): array
+    private static function stabiliseLayers(array $layers): array
     {
         $stabiliser = new LayerStabiliser();
 
@@ -282,24 +188,10 @@ class VolumePacker implements LoggerAwareInterface
     }
 
     /**
-     * As well as purely dimensional constraints, there are other constraints that need to be met
-     * e.g. weight limits or item-specific restrictions (e.g. max <x> batteries per box).
-     */
-    protected function checkNonDimensionalConstraints(Item $itemToPack, array $layers, int $remainingWeightAllowed): bool
-    {
-        $customConstraintsOK = true;
-        if ($itemToPack instanceof ConstrainedItem) {
-            $customConstraintsOK = $itemToPack->canBePackedInBox($this->getPackedItemList($layers), $this->box);
-        }
-
-        return $customConstraintsOK && $itemToPack->getWeight() <= $remainingWeightAllowed;
-    }
-
-    /**
      * Swap back width/length of the packed items to match orientation of the box if needed.
      * @param PackedLayer[] $oldLayers
      */
-    protected static function rotateLayersNinetyDegrees($oldLayers): array
+    private static function rotateLayersNinetyDegrees($oldLayers): array
     {
         $newLayers = [];
         foreach ($oldLayers as $originalLayer) {
@@ -316,8 +208,9 @@ class VolumePacker implements LoggerAwareInterface
 
     /**
      * Generate a single list of items packed.
+     * @param PackedLayer[] $layers
      */
-    protected function getPackedItemList(array $layers): PackedItemList
+    private function getPackedItemList(array $layers): PackedItemList
     {
         $packedItemList = new PackedItemList();
         foreach ($layers as $layer) {
@@ -333,7 +226,7 @@ class VolumePacker implements LoggerAwareInterface
      * Return the current packed depth.
      * @param PackedLayer[] $layers
      */
-    protected static function getCurrentPackedDepth(array $layers): int
+    private static function getCurrentPackedDepth(array $layers): int
     {
         $depth = 0;
         foreach ($layers as $layer) {
@@ -341,31 +234,5 @@ class VolumePacker implements LoggerAwareInterface
         }
 
         return $depth;
-    }
-
-    private function getRemainingWeightAllowed(array $layers): int
-    {
-        $remainingWeightAllowed = $this->box->getMaxWeight() - $this->box->getEmptyWeight();
-        foreach ($layers as $layer) {
-            $remainingWeightAllowed -= $layer->getWeight();
-        }
-
-        return $remainingWeightAllowed;
-    }
-
-    /**
-     * Compare two items to see if they have same dimensions.
-     */
-    protected static function isSameDimensions(Item $itemA, Item $itemB): bool
-    {
-        if ($itemA === $itemB) {
-            return true;
-        }
-        $itemADimensions = [$itemA->getWidth(), $itemA->getLength(), $itemA->getDepth()];
-        $itemBDimensions = [$itemB->getWidth(), $itemB->getLength(), $itemB->getDepth()];
-        sort($itemADimensions);
-        sort($itemBDimensions);
-
-        return $itemADimensions === $itemBDimensions;
     }
 }
