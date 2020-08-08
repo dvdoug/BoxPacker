bf12968d2010ea10791e10011ac8a43709dc6e0f
Doug Wright <github@dougweb.org>
2020-08-08 20:14:48 +0100

    Tighten up types on efficiency test data provider

--- tests/EfficiencyTest.php
+++ tests/EfficiencyTest.php
@@ -25,15 +25,15 @@ class EfficiencyTest extends TestCase
      * @group efficiency
      */
     public function testCanPackRepresentativeLargerSamples(
-        $test,
-        $boxes,
-        $items,
-        $expectedBoxes2D,
-        $expectedBoxes3D,
-        $expectedWeightVariance2D,
-        $expectedWeightVariance3D,
-        $expectedVolumeUtilisation2D,
-        $expectedVolumeUtilisation3D
+        string $test,
+        array $boxes,
+        array $items,
+        int $expectedBoxes2D,
+        int $expectedBoxes3D,
+        float $expectedWeightVariance2D,
+        float $expectedWeightVariance3D,
+        float $expectedVolumeUtilisation2D,
+        float $expectedVolumeUtilisation3D
     ): void {
         $expectedItemCount = 0;
 
@@ -50,10 +50,10 @@ class EfficiencyTest extends TestCase
             $packer2D->addItem(
                 new TestItem(
                     $item['name'],
-                    (int) $item['width'],
-                    (int) $item['length'],
-                    (int) $item['depth'],
-                    (int) $item['weight'],
+                    $item['width'],
+                    $item['length'],
+                    $item['depth'],
+                    $item['weight'],
                     TestItem::ROTATION_KEEP_FLAT
                 ),
                 (int) $item['qty']
@@ -62,10 +62,10 @@ class EfficiencyTest extends TestCase
             $packer3D->addItem(
                 new TestItem(
                     $item['name'],
-                    (int) $item['width'],
-                    (int) $item['length'],
-                    (int) $item['depth'],
-                    (int) $item['weight'],
+                    $item['width'],
+                    $item['length'],
+                    $item['depth'],
+                    $item['weight'],
                     TestItem::ROTATION_BEST_FIT
                 ),
                 (int) $item['qty']
@@ -95,7 +95,7 @@ class EfficiencyTest extends TestCase
         self::assertEquals($expectedWeightVariance3D, $packedBoxes3D->getWeightVariance());
     }
 
-    public function getSamples()
+    public function getSamples(): array
     {
         $expected = ['2D' => [], '3D' => []];
 
@@ -128,12 +128,12 @@ class EfficiencyTest extends TestCase
         while ($data = fgetcsv($itemData)) {
             if (isset($tests[$data[0]])) {
                 $tests[$data[0]]['items'][] = [
-                    'qty' => $data[1],
+                    'qty' => (int) $data[1],
                     'name' => $data[2],
-                    'width' => $data[3],
-                    'length' => $data[4],
-                    'depth' => $data[5],
-                    'weight' => $data[6],
+                    'width' => (int) $data[3],
+                    'length' => (int) $data[4],
+                    'depth' => (int) $data[5],
+                    'weight' => (int) $data[6],
                 ];
             } else {
                 $tests[$data[0]] = [
@@ -141,20 +141,20 @@ class EfficiencyTest extends TestCase
                     'boxes' => $boxes,
                     'items' => [
                         [
-                            'qty' => $data[1],
+                            'qty' => (int) $data[1],
                             'name' => $data[2],
-                            'width' => $data[3],
-                            'length' => $data[4],
-                            'depth' => $data[5],
-                            'weight' => $data[6],
+                            'width' => (int) $data[3],
+                            'length' => (int) $data[4],
+                            'depth' => (int) $data[5],
+                            'weight' => (int) $data[6],
                         ],
                     ],
                     'expected2D' => (int) $expected['2D'][$data[0]]['boxes'],
                     'expected3D' => (int) $expected['3D'][$data[0]]['boxes'],
-                    'weightVariance2D' => $expected['2D'][$data[0]]['weightVariance'],
-                    'weightVariance3D' => $expected['3D'][$data[0]]['weightVariance'],
-                    'volumeUtilisation2D' => $expected['2D'][$data[0]]['utilisation'],
-                    'volumeUtilisation3D' => $expected['3D'][$data[0]]['utilisation'],
+                    'weightVariance2D' => (float) $expected['2D'][$data[0]]['weightVariance'],
+                    'weightVariance3D' => (float) $expected['3D'][$data[0]]['weightVariance'],
+                    'volumeUtilisation2D' => (float) $expected['2D'][$data[0]]['utilisation'],
+                    'volumeUtilisation3D' => (float) $expected['3D'][$data[0]]['utilisation'],
                 ];
             }
         }
